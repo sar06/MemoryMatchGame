2d8ce1c10d0f2508254c092aff0d58a3997c6645
sar06 <48696842+sar06@users.noreply.github.com>
2019-03-19 23:15:38 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import GameManager from "./container/GameManager"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <GameManager />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/README.md
@@ -0,0 +1,2 @@
+# MemoryMatchGame
+# MemoryMatchGame
